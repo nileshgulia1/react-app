44db9a1c4d3a78849beac34a5024140b2fbfdfe6
nilesh <nileshgulia@gmail.com>
2017-09-16 20:07:37 +0530

    game outlook

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,9 +7,21 @@ class Quiz extends Component {
             <div className="quiz">
              <div className="quiz-content">
               <p className="question">What is the sum of <span className="text-info" >5</span> and <span className="text-info" >10</span>?</p>
+              <div className="options">
+               <div className="fields"><div className="field-block">10</div></div>
+               <div className="fields"><div className="field-block">10</div></div>
+               <div className="fields"><div className="field-block">10</div></div>
+               <div className="fields"><div className="field-block">10</div></div>
+              </div>
+             </div>
+               
+              
+             
+             <div className="play-again">
+             <a className="button">Play again</a>
+             
              
              </div>
-            
             
             
             
