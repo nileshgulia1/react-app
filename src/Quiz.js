48316d7bfbc22e3eeac199237e30f2d413e7643f
nilesh <nileshgulia@gmail.com>
2017-09-16 18:10:55 +0530

    initialise a Game

---
+++ src/Quiz.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Quiz extends Component {
+    render(){
+        return(
+            <div className="quiz">
+            
+            
+            
+            
+            </div>
+            
+
+
+
+
+        );
+    }
+}
+export default Quiz;
